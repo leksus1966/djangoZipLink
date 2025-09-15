a5dd09be60511fa032541ae1f8b31566df6c9ca0
leksus1966 <alexsokalo@gmail.com>
2025-09-15 19:41:42 +0300

    add models and serializers

---
+++ shortener/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import ShortURL
+
+
+class ShortURLSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShortURL
+        fields = ["id", "original_url", "short_code", "created_at"]
+        read_only_fields = ["short_code", "created_at"]
